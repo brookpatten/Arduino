c84503a3451e2d7f2a4d5fc7840a534a6c84d98d
Brook Patten <brook.patten@gmail.com>
2016-12-10 23:10:37 -0500

    working speedo

--- gps-speedometer/gps-speedometer.ino
+++ gps-speedometer/gps-speedometer.ino
@@ -37,6 +37,7 @@ SoftwareSerial gpsSerial(8, 7);  // GPS breakout/shield will use a
                                  // TX = pin 8 and RX = pin 7.
 Adafruit_GPS gps(&gpsSerial);
 float maxKnots = 0;
+int counter=0;
 
 
 void setup() {
@@ -44,7 +45,9 @@ void setup() {
 
   // Setup Serial port to print debug output.
   Serial.begin(115200);
-  Serial.println("Clock starting!");
+  Serial.println("Speedometer starting!");
+
+  i2cScan();
 
   // Setup the display.
   clockDisplay.begin(DISPLAY_ADDRESS);
@@ -57,10 +60,12 @@ void setup() {
   gps.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
 
   // Use a 1 hz, once a second, update rate.
-  gps.sendCommand(PMTK_SET_NMEA_UPDATE_5HZ);
+  gps.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
   
   // Enable the interrupt to parse GPS data.
   enableGPSInterrupt();
+
+  
 }
 
 void loop() {
@@ -69,33 +74,56 @@ void loop() {
   // Check if GPS has new data and parse it.
   if (gps.newNMEAreceived()) {
     gps.parse(gps.lastNMEA());
+
+    if(gps.satellites<5)
+    {
+      Serial.print("Satellites:");
+      Serial.println(gps.satellites,DEC);
+      Serial.print("Fix:");
+      Serial.println(gps.fixquality,DEC);
+      //Serial.println(gps.lastNMEA());
+      clockDisplay.println(gps.satellites, DEC);
+      clockDisplay.writeDisplay();
+    }
+    else
+    {
+      float knots = gps.speed;
+    
+      if(knots>maxKnots){
+        maxKnots = knots;
+      }
+    
+      if(false){ //max button is down
+        knots = maxKnots;
+      }
+      
+      float mph = knots * 1.15078;
+    
+      int displayValue = (int)mph;
+
+      //displayValue = counter;
+      //counter++;
+      //if(counter>99)
+      //{
+      //  counter = 0;
+      //}
+    
+      Serial.print("Speed:");
+      Serial.println(displayValue,DEC);
+      
+      clockDisplay.println(displayValue, DEC);
+      clockDisplay.writeDisplay();
+
+      //delay(1000);
+    }
+  }
+  else
+  {
+    clockDisplay.println(0, DEC);
+    clockDisplay.writeDisplay();
   }
 
-  float knots = gps.speed;
-
-  if(knots>maxKnots){
-    maxKnots = knots;
-  }
-
-  if(false){ //max button is down
-    knots = maxKnots;
-  }
   
-  float mph = knots * 1.15078;
-  int displayValue = (int)mph;
-
-  //just for debugging purposes
-  displayValue = 67;
-
-  // Now print the time value to the display.
-  clockDisplay.print(displayValue, DEC);
-
-  // Now push out to the display the new values that were set above.
-  clockDisplay.writeDisplay();
-
-  // Loop code is finished, it will jump back to the start of the loop
-  // function again! Don't add any delays because the parsing needs to 
-  // happen all the time!
 }
 
 SIGNAL(TIMER0_COMPA_vect) {
@@ -112,4 +140,46 @@ void enableGPSInterrupt() {
   OCR0A = 0xAF;
   TIMSK0 |= _BV(OCIE0A);
 }
+
+void i2cScan()
+ {
+   byte error, address;
+   int nDevices;
+   Wire.begin();
+  Serial.println("Scanning...");
+ 
+  nDevices = 0;
+   for(address = 0; address <= 127; address++ )
+  {
+     // The i2c_scanner uses the return value of
+     // the Write.endTransmisstion to see if
+     // a device did acknowledge to the address.
+     Wire.beginTransmission(address);
+     error = Wire.endTransmission();
+ 
+    if (error == 0)
+     {
+       Serial.print("I2C device found at address 0x");
+       if (address<16)
+        Serial.print("0");
+       Serial.print(address,HEX);
+       Serial.println(" !");
+ 
+      nDevices++;
+     }
+     else if (error==4)
+    {
+       Serial.print("Unknow error at address 0x");
+       if (address<16)
+        Serial.print("0");
+       Serial.println(address,HEX);
+     }
+   }
+   if (nDevices == 0)
+     Serial.println("No I2C devices found\n");
+   else
+     Serial.println("done\n");
+ 
+  //delay(8000);           // wait 8 seconds for next scan
+ }
 
